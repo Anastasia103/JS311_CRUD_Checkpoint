52ae1e06d45e7cc2cea91d72e93b72b3669d3b33
Anastasia103 <anastasia.sutherland12@gmail.com>
2022-09-25 15:56:19 -0400

    did troubleshooting on controllers, and added comments, ALL BOOK ROUTES WORKING

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -1,5 +1,6 @@
 const db = require('../utils/db')
 
+// shows all the books in the books table
 let getAllBooks = function(req, res){
     let sql = "select * from books"
     db.query(sql, function(err, rows){
@@ -7,13 +8,24 @@ let getAllBooks = function(req, res){
         {console.log("list todos query failed", err)
         res.sendStatus(500)
         } else{
-        res.json(rows)
+        // this function changes the table so that the haveRead column shows yes if the value is 0 in that column and no if the value 
+        // is 1, this is for the users benefits, does not change the table in the data base    
+        let newRows = rows
+        for (let i = 0; i < rows.length; i++) {
+            if (newRows[i].haveRead == 0){
+                newRows[i].haveRead = "no"
+        } else {
+            newRows[i].haveRead = "yes"
+        }
+        }   
+        res.json(newRows)
         }
     })
 }
 
+// this shows one book from table given an id, also shows yes or no for the HaveRead column similar to getAllbooks
 let getBook = function(req, res){
-    let id = request.params.id
+    let id = req.params.id
     if(!id){
         res.sendStatus(400)
         return
@@ -33,21 +45,30 @@ let getBook = function(req, res){
         } else {
             let row = rows[0]
             if (row.haveRead == 0){
-                row.done = "yes"
+                row.haveRead = "no"
             } else {
-                row.done = "no"
+                row.haveRead = "yes"
             }
             res.json(row)
         }       
     })
 }
 
+// updates the book, if the request has name, author, and haveRead in the request body and id in the path
 let updateBook = function(req, res) {
-    let sql = "UPDATE book SET name = ?, author = ?, haveRead = ? WHERE id = ?"
+    let sql = "UPDATE books SET name = ?, author = ?, haveRead = ? WHERE id = ?"
+    // this piece of code makes it so that the user can input no or yes for the haveRead column and this translates the repsonse, and then
+    // translates the answere to 0 or 1, so the information can be recorded in the data base in the correct format.
+    let translatedhaveRead
+    if(req.body.haveRead == "yes"){
+        translatedhaveRead = "1"
+    } else {
+        translatedhaveRead = "0"
+    }
     let params = [
         req.body.name,
         req.body.author,
-        req.body.haveRead,
+        translatedhaveRead,
         req.params.id
     ]
     db.query (sql, params, function(err, rows){
@@ -73,16 +94,18 @@ let createBook = function(req, res) {
          console.log("failure to complete request", err)
          res.sendStatus(500)   
         } else {
+            // this function only outputs a 204 message (might change later) if the request goes through
             console.log("Item created", rows)
             res.sendStatus(204)
         }
     })
 }
 
+// this function deletes a book given an id
 let deleteBook = function(req, res) {
-    let sql = "DELETE books where id = ?"
+    let sql = "DELETE FROM books WHERE id = ?"
     let params = [
-        request.params.id
+        req.params.id
     ]
     db.query (sql, params, function(err, rows) {
         if (err) {
@@ -90,11 +113,11 @@ let deleteBook = function(req, res) {
             res.sendStatus(500)
         } else {
             console.log("Item deleted", rows)
-            res.json(rows.affectedRows)
+            res.sendStatus(204)
         }
     })   
 }
-
+// exporting all the controllers to the bookRoutes file
 module.exports = {
     getAllBooks, 
     getBook, 
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,9 +3,13 @@ let express = require("express")
 let router = express.Router()
 let controller = require('../controller/bookController')
 
-router.get("books", controller.getAllBooks)
-router.get("books/:id", controller.getBook)
 
-router.delete("books/:id", controller.deleteBook)
-router.put("books/:id", controller.updateBook)
-router.post("books/", controller.createBook)
+// my Routes, see bookcontrollers for details on specifics
+router.get("/books", controller.getAllBooks)
+router.get("/books/:id", controller.getBook)
+
+router.delete("/books/:id", controller.deleteBook)
+router.put("/books/:id", controller.updateBook)
+router.post("/books", controller.createBook)
+
+module.exports = router
